8bb135b7726e0db8931238e4cf02ec5a7440e4d1
IceYGO <iceygo@openmailbox.org>
2016-06-19 06:35:26 +0100

    Add two helper functions to YGOClient

--- YGOSharp.Network/YGOClient.cs
+++ YGOSharp.Network/YGOClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using YGOSharp.Network.Enums;
 
 namespace YGOSharp.Network
 {
@@ -17,6 +18,25 @@ namespace YGOSharp.Network
         public void Send(BinaryWriter writer)
         {
             Send(((MemoryStream)writer.BaseStream).ToArray());
+        }
+
+        public void Send(CtosMessage message)
+        {
+            using (BinaryWriter writer = new BinaryWriter(new MemoryStream()))
+            {
+                writer.Write((byte)message);
+                Send(writer);
+            }
+        }
+
+        public void Send(CtosMessage message, int value)
+        {
+            using (BinaryWriter writer = new BinaryWriter(new MemoryStream()))
+            {
+                writer.Write((byte)message);
+                writer.Write(value);
+                Send(writer);
+            }
         }
     }
 }
