572233b605a71abbc80f0a5e1a93b1c34c02ea78
IceYGO <iceygo@openmailbox.org>
2016-06-19 06:39:57 +0100

    Bump pro version to 1339 and update readme

--- YGOSharp/Program.cs
+++ YGOSharp/Program.cs
@@ -9,7 +9,7 @@ namespace YGOSharp
 {
     public class Program
     {
-        public static uint ClientVersion = 0x1338;
+        public static uint ClientVersion = 0x1339;
 
         public static void Main(string[] args)
         {
