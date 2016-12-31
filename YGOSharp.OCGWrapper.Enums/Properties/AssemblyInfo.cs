bdd6429239819253527da0dfddb8c303f2070b80
IceYGO <iceygo@openmailbox.org>
2016-12-31 20:40:13 +0100

    Happy New Year (2017)

--- YGOSharp.OCGWrapper.Enums/Properties/AssemblyInfo.cs
+++ YGOSharp.OCGWrapper.Enums/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IceYGO")]
 [assembly: AssemblyProduct("YGOSharp.OCGWrapper.Enums")]
-[assembly: AssemblyCopyright("Copyright © IceYGO 2015-2016")]
+[assembly: AssemblyCopyright("Copyright © IceYGO 2015-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
